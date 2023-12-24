6353eaaf7e2ed4ab5a3bb96a2922892653d2138e
ammarkh <ammarkh@umich.edu>
2023-12-23 22:06:48 -0500

    Working example finished. Adds hardcoded entry to To-Do database in Notion

---
+++ backend.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+// Notion SDK for JavaScript
+const { Client } = require("@notionhq/client");
+const notion = new Client({ auth: process.env.NOTION_KEY });
+
+// <http://expressjs.com/en/starter/static-files.html>
+app.use(express.static("public"));
+app.use(express.json()) // for parsing application/json
+
+
+// <http://expressjs.com/en/starter/basic-routing.html>
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/templates/index.html");
+});
+
+// listen for requests
+const listener = app.listen(process.env.PORT, function () {
+    console.log("Your app is listening on port " + listener.address().port);
+});
+
+app.post("/databases", async function (request, response) {
+
+    console.log(request.body);
+    const pageId = process.env.NOTION_PAGE_ID;
+    const task_ = request.body.task
+    const class_ = request.body.class
+    const dueDate_ = request.body.dueDate
+    const notes_ = request.body.notes
+
+    try {
+        // Notion API request!
+        const response = await notion.pages.create({
+            "parent": {
+                "type": "database_id",
+                "database_id": "9685de97-a81d-4c81-92d4-e7cb37b8b8a0" //hardcoded to To-Do database
+            },
+            "properties": {
+                "Task": {
+                    "title": [
+                        {
+                            "text": {
+                                "content": task_
+                            }
+                        }
+                    ]
+                },
+                "Class": {
+                    "relation": [
+                        {
+                            "id": "71e393d7-cafc-4c7a-b89a-6c4f0eedabc6"  //hardcoded to Test class 
+                        }
+                    ],
+                    "has_more": false
+                },
+                "Due": {
+                    "date": {
+                        "start": "2020-12-08T12:00:00Z"
+                    }
+                },
+                "Notes": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": "Test note"
+                            }
+                        }
+                    ]
+                }
+            }
+        });
+
+        response.json({ message: "success!", data: response });
+    } catch (error) {
+        response.json({ message: "error", error });
+    }
+});
---
+++ public/frontend.js
@@ -0,0 +1,61 @@
+const appendApiResponse = function (apiResponse, el) {
+  console.log(apiResponse)
+
+  // Add success message to UI
+  const newParagraphSuccessMsg = document.createElement("p")
+  newParagraphSuccessMsg.innerHTML = "Result: " + apiResponse.message
+  el.appendChild(newParagraphSuccessMsg)
+  // See browser console for more information
+  if (apiResponse.message === "error") return
+
+  // Add ID of Notion item (db, page, comment) to UI
+  const newParagraphId = document.createElement("p")
+  newParagraphId.innerHTML = "ID: " + apiResponse.data.id
+  el.appendChild(newParagraphId)
+
+  // Add URL of Notion item (db, page) to UI
+  if (apiResponse.data.url) {
+    const newAnchorTag = document.createElement("a")
+    newAnchorTag.setAttribute("href", apiResponse.data.url)
+    newAnchorTag.innerText = apiResponse.data.url
+    el.appendChild(newAnchorTag)
+  }
+}
+
+// Assign the database form to a variable for later use
+const toDoForm = document.getElementById("toDoForm");
+// Assign the empty table cell to a variable for later use
+const dbResponseEl = document.getElementById("dbResponse");
+
+// Add a submit handler to the form
+toDoForm.onsubmit = async function (event) {
+  event.preventDefault()
+
+// Get the database name from the form
+  console.log(event.target.taskVal.value)
+  const task_ = event.target.taskVal.value
+  const class_ = event.target.classVal.value
+  const dueDate_ = event.target.dueDateVal.value
+  const notes_ = event.target.notesVal.value
+
+  const body = JSON.stringify({ 
+    task: task_,
+    class: class_,
+    dueDate: dueDate_,
+    notes: notes_
+  })
+
+// Make a request to /databases endpoint in server.js
+  const newDBResponse = await fetch("/databases", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  })
+  const newDBData = await newDBResponse.json()
+
+// Pass the new database info and the empty table cell
+// to a function that will append it.
+  console.log(newDBData)
+}
