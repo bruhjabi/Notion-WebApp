0146c7fabe22690fc10ad035626a3864a55958b3
GamerWiz <ammarahmedkhan29@gmail.com>
2023-12-30 09:56:28 -0500

    EJS templating stable, classes are now loaded dynamically

--- backend.js
+++ backend.js
@@ -22,18 +22,17 @@ app.set('views', path.join(__dirname, '/templates'));
 //  https://developers.notion.com/reference/post-database-query
 app.get("/", async function (request, response) {
     try {
-        const response = await notion.databases.query({
+        const notionData = await notion.databases.query({
             database_id: "ef87ade3-24e5-44ed-93fb-e55d04e224f7"
         })
-        const len = response.results.length;
-        console.log(len);
-        let classesList = [];
+        const len = notionData.results.length;
+        let classesList = {};
         for (let index = 0; index < len; index++) {
-            classesList[index] = response.results[index].properties.Name.title[0].plain_text;
+            classesList[notionData.results[index].properties.Name.title[0].plain_text] = notionData.results[index].id;
         }
-        console.log(classesList);
-        response.render(index, {classesList})
+        response.render("index", {classesList});
     } catch (error) {
+        console.log(error);
         response.json({ message: "error", error });
     }
 });
@@ -52,10 +51,10 @@ app.post("/api", async function (request, response) {
 
     try {
         // Notion API request!
-        const response = await notion.pages.create({
+        const notionRequest = await notion.pages.create({
             "parent": {
                 "type": "database_id",
-                "database_id": "9685de97-a81d-4c81-92d4-e7cb37b8b8a0" //hardcoded to To-Do database
+                "database_id": pageId //hardcoded to To-Do database
             },
             "properties": {
                 "Task": {
@@ -70,14 +69,14 @@ app.post("/api", async function (request, response) {
                 "Class": {
                     "relation": [
                         {
-                            "id": "71e393d7-cafc-4c7a-b89a-6c4f0eedabc6"  //hardcoded to Test class 
+                            "id": class_                  
                         }
                     ],
                     "has_more": false
                 },
                 "Due": {
                     "date": {
-                        "start": "2020-12-08T12:00:00Z"
+                        "start": dueDate_
                     }
                 },
                 "Notes": {
@@ -85,7 +84,7 @@ app.post("/api", async function (request, response) {
                         {
                             "type": "text",
                             "text": {
-                                "content": "Test note"
+                                "content": notes_
                             }
                         }
                     ]
@@ -93,8 +92,9 @@ app.post("/api", async function (request, response) {
             }
         });
 
-        response.json({ message: "success!", data: response });
+        response.json({ message: "success!"});
     } catch (error) {
+        console.log(error)
         response.json({ message: "error", error });
     }
 });
